94019c58549fce7ee07a6e0b2e9e148b20038db2
longg2504 <longg2504@gmail.com>
2023-06-13 15:23:23 +0700

    long upload structured 13-06-2023

---
+++ case_study_3/src/main/java/model/Bill.java
@@ -0,0 +1,83 @@
+package model;
+
+import java.util.Date;
+
+public class Bill {
+    private int bill_id;
+    private User user;
+    private double total;
+    private String payment;
+    private String address;
+    private Date createDate;
+    private String phone;
+
+    public Bill() {
+
+    }
+
+    public Bill(int bill_id, User user, double total, String payment, String address, Date createDate, String phone) {
+        this.bill_id = bill_id;
+        this.user = user;
+        this.total = total;
+        this.payment = payment;
+        this.address = address;
+        this.createDate = createDate;
+        this.phone = phone;
+    }
+
+    public int getBill_id() {
+        return bill_id;
+    }
+
+    public void setBill_id(int bill_id) {
+        this.bill_id = bill_id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
--- case_study_3/src/main/java/model/Product.java
+++ case_study_3/src/main/java/model/Product.java
@@ -1,5 +1,83 @@
 package model;
 
 public class Product {
+    private int id ;
 
+    private Category category;
+
+    private String name;
+    private double price;
+    private String describle;
+    private int quantity;
+    private String image;
+
+    public Product() {
+
+    }
+
+    public Product(int id, Category category, String name, double price, String describle, int quantity, String image) {
+        this.id = id;
+        this.category = category;
+        this.name = name;
+        this.price = price;
+        this.describle = describle;
+        this.quantity = quantity;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescrible() {
+        return describle;
+    }
+
+    public void setDescrible(String describle) {
+        this.describle = describle;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
